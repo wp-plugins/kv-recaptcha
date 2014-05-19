6fc6313eaf55da02e97e6ab1fbe358280b0e89db
kvvaradha <kvvaradha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 04:12:16 +0000

    common update
    git-svn-id: https://plugins.svn.wordpress.org/kv-recaptcha/trunk@916895 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kv_recaptcha.php
+++ kv_recaptcha.php
@@ -91,12 +91,103 @@ function kv_recaptcha() {
     endif;  // end reCaptcha
 
 }
-
+/*
 if(!function_exists('kv_admin_menu_captcha')) {
 	function kv_admin_menu_captcha() { 		
 		add_menu_page('KV reCaptcha', 'KV reCaptcha', 'manage_options', 'kv_recaptcha' , 'kv_recaptcha_admin', KV_RECAPTCHA_URL.'/images/kv_logo.png', 67);	
 	}
 add_action('admin_menu', 'kv_admin_menu_captcha');
+}
+*/
+
+
+if(!function_exists('kv_admin_menu')) {
+	function kv_admin_menu() { 		
+		add_menu_page('Kvcodes', 'Kvcodes', 'manage_options', 'kvcodes' , 'kv_codes_plugins', KV_RECAPTCHA_URL.'/images/kv_logo.png', 66);	
+		add_submenu_page( 'kvcodes', 'KV reCaptcha', 'KV reCaptcha', 'manage_options', 'kv_plugin_hider', 'kv_list_plugins' );
+	}
+add_action('admin_menu', 'kv_admin_menu');
+
+function kv_codes_plugins() {
+
+?>
+ <div class="wrap">
+        <div class="icon32" id="icon-tools"><br/></div>
+        <h2><?php _e('KvCodes', 'kvcodes') ?></h2>		
+		<div class="welcome-panel">
+								<?php //kv_admin_thirty_day_chart () ; ?>
+								Thank you for using Kvcodes Plugins . Here is my few Plugins work .MY plugins are very light weight and Simple.  <p>
+								<a href="http://www.kvcodes.com/" target="_blank" ><h3> Visit My Blog</h3></a></p> 
+								</div> 
+		<div id="poststuff" > 
+			<div id="post-body" class="metabox-holder columns-2" >
+				<div id="post-body-content" > 
+					<div class="meta-box-sortables"> 
+						<div id="dashboard_right_now" class="postbox">
+							<div class="handlediv" > <br> </div>
+							<h3 class="hndle"  ><img src="<?php echo KV_RECAPTCHA_URL.'/images/kv_logo.png'; ?>" >  My plugins </h3> 
+							<div class="inside" style="padding: 10px; "> 
+								
+								<?php 
+									$kv_wp =  kv_get_web_page('http://profiles.wordpress.org/kvvaradha'); 
+									
+									 $kv_first_pos = strpos($kv_wp['content'], '<div id="content-plugins" class="info-group plugin-theme main-plugins inactive">');
+									
+									$kv_first_trim = substr($kv_wp['content'] , $kv_first_pos ); 
+										
+									$kv_sec_pos = strpos($kv_first_trim, '</div>');
+									
+									$kv_sec_trim = substr($kv_first_trim ,0, $kv_sec_pos );  
+									
+									echo $kv_sec_trim; 					?> 
+								</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div> 
+
+</div> <!-- /wrap -->
+<?php
+
+}
+
+function kv_get_web_page( $url )
+{
+	$options = array(
+		CURLOPT_RETURNTRANSFER => true,     // return web page
+		CURLOPT_HEADER         => false,    // don't return headers
+		CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+		CURLOPT_ENCODING       => "",       // handle compressed
+		CURLOPT_USERAGENT      => "spider", // who am i
+		CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+		CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+		CURLOPT_TIMEOUT        => 120,      // timeout on response
+		CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+	);
+
+	$ch      = curl_init( $url );
+	curl_setopt_array( $ch, $options );
+	$content = curl_exec( $ch );
+	$err     = curl_errno( $ch );
+	$errmsg  = curl_error( $ch );
+	$header  = curl_getinfo( $ch );
+	curl_close( $ch );
+
+	$header['errno']   = $err;
+	$header['errmsg']  = $errmsg;
+	$header['content'] = $content;
+	return $header;
+}
+
+} else {
+	function kv_admin_submenu_recaptcha() { 		
+		add_submenu_page( 'kvcodes', 'KV reCaptcha', 'KV reCaptcha', 'manage_options', 'kv_plugin_hider', 'kv_list_plugins' );
+	}
+add_action('admin_menu', 'kv_admin_submenu_recaptcha');
+	
 }
 
 add_action('admin_init', 'kv_admin_recaptcha_register');
@@ -109,10 +200,7 @@ function kv_admin_recaptcha_register() {
 	//register_setting('kv_recaptcha' , 'kv_recaptcha_theme');
 }
 
-function kv_recaptcha_admin() {
-
-	
-?>
+function kv_recaptcha_admin() { ?>
  <div class="wrap">
         <div class="icon32" id="icon-tools"><br/></div>
         <h2><?php _e('Kv reCaptcha', 'kv_recaptcha') ?></h2>
@@ -170,37 +258,6 @@ function kv_recaptcha_admin() {
 						</div> 
 					</div>
 				</div> 
-				
-				<div id="postbox-container-2" class="postbox-container" > 
-					<div class="meta-box-sortables"> 
-						<div id="postbox-container-2" class="postbox-container" > 
-						
-						<div id="dashboard_right_now" class="postbox">
-							<div class="handlediv" > <br> </div>
-							<h3 class="hndle" > Donate </h3> 
-							<div class="inside"  style="padding: 10px; "> 
-							<b>If i helped you, you can buy me a coffee, just press the donation button :)</b> 
-							<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-								<input type="hidden" name="cmd" value="_donations" />
-								<input type="hidden" name="business" value="<?php echo 'kvvaradha@gmail.com'; ?>" />
-								<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-								<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-							</form>
-							</div> 
-						</div> 
-						</div>
-						<div id="postbox-container-2" class="postbox-container" > 
-						<div id="dashboard_quick_press" class="postbox">
-							<div class="handlediv" > <br> </div>
-							<h3 class="hndle" > Support me from Facebook </h3> 
-							<div class="inside"  style="padding: 10px; "> 
-							<p><iframe allowtransparency="true" frameborder="0" scrolling="no" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fkvcodes&amp;width=180&amp;height=300&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=false&amp;appId=117935585037426" style="border:none; overflow:hidden; width:250px; height:300px;"></iframe></p>
-							</div> 
-						</div> 
-						</div>
-					</div>
-				</div> 
-				
 			</div>
 		</div> 
 </div> <!-- /wrap -->
